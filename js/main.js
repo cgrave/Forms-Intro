19c30b06ae334ee4d18c0278369e059f812d8d60
Crystal <33204245+cgrave@users.noreply.github.com>
2018-02-07 08:12:36 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,14 +2,24 @@
 document.querySelector("form.login-form").addEventListener("submit", function (evt){
 	var username = evt.target.elements.username
 	var password = evt.target.elements.password
+	var valid = true;
 
-// trim() removes whitespace on left and right
-	if(username.value.trim() == ""){
+	if(username.value.trim() == '') {
+		valid = false;
+		username.classList.add('error');
+	} else {
+		username.classList.remove('error');
+	}
+
+	if(password.value.trim().length < 8) {
+		valid = false;
+		password.classList.add('error');
+	} else {
+		password.classList.remove('error');
+	}
+
+	if(valid == false) { // if(!valid) {
 		evt.preventDefault();
 	}
-	if(password.value.trim().length){
 
-	}
-	evt.preventDefault();
-})
-
+});
